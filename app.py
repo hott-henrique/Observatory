a285e0bb5f093ae3844d976829355e158b0b1b84
Henrique Hott <cs.henriquemchott@gmail.com>
2023-10-02 07:18:36 -0300

    BUGFIX: getattr not given default value.

--- app.py
+++ app.py
@@ -6,20 +6,18 @@ import api
 
 
 def create_app():
-    os.makedirs(".STORAGE", exist_ok=True)
-
     app = flask.Flask(__name__)
 
     app.register_blueprint(api.bp)
 
     @app.teardown_appcontext
     def close_connection(_):
-        f = getattr(flask.g, "_APP_LOGFILE")
+        f = getattr(flask.g, "_APP_LOGFILE", None)
 
         if f is not None:
             f.close()
 
-        db = getattr(flask.g, "_MONGODB_INSTANCE")
+        db = getattr(flask.g, "_MONGODB_INSTANCE", None)
 
         if db is not None:
             db.close()
