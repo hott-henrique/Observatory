05a6bbec96a63bf9bcc08532b34f0c0e7aab6852
Guilherme <bittenkourt.gmf@gmail.com>
2023-12-04 21:08:10 -0300

    Main request fixed.

--- app/main.py
+++ app/main.py
@@ -20,12 +20,6 @@ async def lifespan(app: fastapi.FastAPI):
     app.state._QDRANT_CLIENT = qdrant.QdrantClient(url=os.getenv("QDRANT_URL"),
                                                    api_key=os.getenv("QDRANT_TOKEN"))
     
-    try:
-        app.state._BERT_MODEL = torch.load("./out_model/pytorch_model.bin", map_location=torch.device('cpu'))
-    except:
-        raise Exception("Could not load model.")
-
-
     yield
 
     app.state._QDRANT_CLIENT.close()
@@ -33,6 +27,14 @@ async def lifespan(app: fastapi.FastAPI):
     app.state._MONGO_CLIENT.close()
 
 app = fastapi.FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    fastapi.middleware.cors.CORSMiddleware,
+    allow_origins=["*"],  # Permitir solicitações de qualquer origem
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os cabeçalhos
+)
 
 app.include_router(news.router)
 app.include_router(embeddings.router)
